ef72bcf0c183f45d533c687cc997851d9253b7f6
Abubokkor98 <mail.abubokkor@gmail.com>
2025-03-17 12:51:43 +0600

    REST API with CORS, Rate Limits, Server Logs, & Exception Filters

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -7,16 +7,21 @@ import {
   Param,
   Delete,
   Query,
+  Ip,
 } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
-import { Prisma } from '@prisma/client';
+import { Prisma, Role } from '@prisma/client';
 import { SkipThrottle, Throttle } from '@nestjs/throttler';
+import { MyLoggerService } from 'src/my-logger/my-logger.service';
 
 // over write throttle
 @SkipThrottle()
 @Controller('employees')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
+
+  // add logger(from last lesson)
+  private readonly logger = new MyLoggerService(EmployeesController.name);
 
   @Post()
   create(@Body() createEmployeeDto: Prisma.EmployeeCreateInput) {
@@ -26,7 +31,9 @@ export class EmployeesController {
   // over write throttle (make default value false so throttle don't skip in this route)
   @SkipThrottle({ default: false })
   @Get()
-  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+  // add logger(from last lesson) also want to get the Ip address
+  findAll(@Ip() ip: string, @Query('role') role?: Role) {
+    this.logger.log(`Request for All Employees\t${ip}`,EmployeesController.name);
     return this.employeesService.findAll(role);
   }
 
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionFilter } from './all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // from lesson 6
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionFilter(httpAdapter));
+
   app.enableCors();
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
